3f5313a6993da692b1b921a00f880bfa05d6b43e
DarrenKwonDev <anatomy1545@gmail.com>
2020-11-22 03:36:02 +0900

    testing update

---
+++ src/restaurants/dtos/update-restaurant.dto.ts
@@ -0,0 +1,15 @@
+import { ArgsType, Field, InputType, PartialType } from '@nestjs/graphql';
+import { Restaurant } from '../entities/restaurant.entity';
+import { CreateRestaurantDto } from './create-restaurant.dto';
+
+@InputType()
+class UpdateRestaurantInputType extends PartialType(CreateRestaurantDto) {}
+
+@ArgsType()
+export class UpdateRestaurantDto {
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => UpdateRestaurantInputType)
+  data: UpdateRestaurantInputType;
+}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -15,10 +16,12 @@ export class RestaurantService {
     return this.restaurant.find();
   }
 
-  createRestaurant(
-    createRestaurantDto: CreateRestaurantDto,
-  ): Promise<Restaurant> {
+  createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
     const newRestaurant = this.restaurant.create(createRestaurantDto);
     return this.restaurant.save(newRestaurant);
+  }
+
+  updateRestaurant({ id, data }: UpdateRestaurantDto) {
+    return this.restaurant.update(id, { ...data }); // caution! : Does not check if entity exist in the database. 즉 없는 id를 넣어도 에러를 안 냄
   }
 }
