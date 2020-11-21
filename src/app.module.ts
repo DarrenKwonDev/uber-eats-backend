057044493b55f538981bc9a4cfb78973f76e235e
DarrenKwonDev <anatomy1545@gmail.com>
2020-11-21 18:54:25 +0900

    gql and enetity migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import Joi from 'joi'; // export= 방시으로 되어 있어서 esModuleInterop를 true로 켜줌
+import Joi from 'joi'; // export= 방식으로 되어 있어서 esModuleInterop를 true로 켜줌
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -27,12 +29,12 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [],
       synchronize: process.env.NODE_ENV === 'prod' ? false : true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
       logging: true,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
-      autoSchemaFile: true, // in memory
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // set true if you want to use in memory gql
       debug: true,
       playground: true,
     }),
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType() // nest
+@Entity() // typeORM
 export class Restaurant {
-  @Field(() => String)
-  name: string;
+  @Field(() => Number) // nest(gql)
+  @PrimaryGeneratedColumn() // typeORM
+  id: number;
+
+  @Field(() => String) // nest(gql)
+  @Column() // typeORM
+  name: string; // nest(typescript)
 
   @Field(() => Boolean)
-  isVegan?: boolean;
+  @Column()
+  isVegan: boolean;
 
   @Field(() => String)
+  @Column()
   address: string;
 
   @Field(() => String)
+  @Column()
   ownersName: string;
+
+  @Field(() => String)
+  @Column()
+  categoryName: string;
 }
