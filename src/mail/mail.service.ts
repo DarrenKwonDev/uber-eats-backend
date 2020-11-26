19f0f5a86dbdf81c000bec8be5019c93d2e079c8
DarrenKwonDev <anatomy1545@gmail.com>
2020-11-27 00:38:27 +0900

    remove mailgun-js, do manually

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,13 +6,10 @@ import { MailModuleOptions } from './mail.interfaces';
 
 @Injectable()
 export class MailService {
-  constructor(@Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions) {
-    console.log(options);
-    this.sendEmail('testing', 'test');
-  }
+  constructor(@Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions) {}
 
   // mailgun 계정 결제를 하지 않았으므로 어차피 자신에게밖에 보내지 못함
-  private async sendEmail(subject: string, content: string, to = 'anatomy1545@gmai.com') {
+  private async sendEmail(subject: string, content: string, to = 'anatomy1545@gmail.com') {
     const form = new FormData(); // 일반 FormData는 안됨. 문서 https://github.com/sindresorhus/got#body 참고
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', to);
@@ -21,10 +18,15 @@ export class MailService {
 
     console.log(Buffer.from(`api: ${this.options.apiKey}`).toString('base64'));
 
+    // 에러가 많이 나네
+    // https://documentation.mailgun.com/en/latest/api-intro.html#errors
+    // 401 (UNAUTHORIZED) No valid API key provided
+    // 400 Bad Request - Often missing a required parameter
+
     try {
       const resonse = await got(`https://api.mailgun.net/v3/${this.options.domain}/messages`, {
         headers: {
-          Authoization: `Basic ${Buffer.from(`api:${this.options.apiKey}`).toString('base64')}`,
+          Authorization: `Basic ${Buffer.from(`api:${this.options.apiKey}`).toString('base64')}`,
         },
         method: 'POST',
         body: form,
